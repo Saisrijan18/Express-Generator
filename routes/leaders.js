b8c96dd3534ba679396de5cd6218023a3a3f0ae9
Saisrijan18 <srijanande123@gmail.com>
2022-06-24 11:31:32 +0530

    Express Generator

---
+++ routes/leaders.js
@@ -0,0 +1,61 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const leaders=express.Router();
+
+leaders.use(bodyParser.json());
+
+leaders.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })
+  
+.get((req,res,next) => {
+      res.end('Will send all the leaders to you!');
+  })
+  
+  .post((req, res, next) => {
+   res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+  })
+  
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leaders');
+  })
+   
+.delete((req, res, next) => {
+      res.end('Deleting all leaders');
+  });
+
+  leaders.route('/:leaderId')
+
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })
+  
+.get((req,res,next) => {
+      res.end('Will send details of leader:'+req.params.leaderId + ' to you!');
+  })
+  
+  .post((req, res, next) => {
+    res.statusCode = 403;
+   res.end('Not supported on /leaders/'+req.params.leaderId );
+  })
+  
+  .put((req, res, next) => {
+    res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    res.end('Will update the leader: ' + req.body.name + 
+          ' with details: ' + req.body.description);
+    
+  })
+   
+.delete((req, res, next) => {
+      res.end('Deleting all leaders');
+  });
+
+
+  module.exports=leaders;
